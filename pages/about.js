4a4863ffdc52036e92606362d7afc13b8eddb22b
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-04-10 22:04:24 +0800

    revised

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import { FaLaptop, FaTwitter } from "react-icons/fa";
@@ -18,20 +19,17 @@ function about({ topics }) {
       <Navbar topics={topics} />
       <div className="max-w-2xl pt-20 pb-32 px-6 text-center mx-auto">
         <div className="h-64 w-full justify-center flex">
-          <img src="/about.png" className="h-64" />
+          <Image src="/about.png" className="h-64" alt="about photo" />
         </div>
         <h2 className="text-3xl font-semibold text-gray-700 py-4 dark:text-gray-200">
-          Hi,{" "}
+          Hi,
           <span className="bg-indigo-400 text-white rounded px-1 dark:bg-indigo-500">
-            I’m Soumyajit
-          </span>{" "}
+            My name is Nirel
+          </span>
           . Nice to meet you.
         </h2>
         <p className="text-gray-600 mt-4 dark:text-indigo-200">
-          I am a self taught developer currently pursuing IMSC in Maths and
-          Computing in BIT Mesra. My field of Interest's are building new Web
-          Technologies and Products and also in areas related to Deep Learning
-          and Natural Launguage Processing.
+          I am a software engineer.
         </p>
 
         <div className="text-center pt-8">
@@ -40,21 +38,9 @@ function about({ topics }) {
             <a
               rel="noopener noreferrer"
               target="_blank"
-              href="https://soumyajit.vercel.app/"
+              href="https://nibarra-portfolio.vercel.app/"
             >
               See My Works
-            </a>
-          </button>
-
-          <button className="bg-indigo-500 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded ml-3 dark:bg-indigo-600">
-            <FaTwitter />
-            <a
-              className="twitter-follow-button"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://twitter.com/soumyajit4419"
-            >
-              Follow Me
             </a>
           </button>
         </div>
